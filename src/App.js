9791e8a1adb8c3c47c267fa1b87c4a6516d69c60
Mike Mwanje <mwanjemike767@gmail.com>
2021-01-23 13:40:29 +0300

    Directory change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. OK!
         </p>
+        <p>
+          My challenge solution
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
